d4992a3c7e20e591ceb736c8c98645baa9ad1fe6
tutku ayavefe <tutku.ayavefe@accenture.com>
2020-05-26 18:42:04 +0300

    Initial Commit

---
+++ Sayı Tahmin Oyunu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sayı Tahmin Oyunu
+//
+//  Created by tutku ayavefe on 26/05/2020.
+//  Copyright © 2020 tutku ayavefe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
